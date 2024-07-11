445018eb34921424915b4f63c7011ff5fe1ed8e2
Gleb Kolebanov <grkolebanov@edu.hse.ru>
2024-07-11 17:45:47 +0300

    Initial commit

---
+++ cmd/urlShortener/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/GlebusDev/urlShortener/internal/config"
+)
+
+func main() {
+	// config cleanenv
+	var config = config.MustLoad()
+
+	// logger slog
+
+	// storage sqlite
+
+	// router chi
+
+	// server
+}
