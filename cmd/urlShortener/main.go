48fe8b254a3c6e550713fdc7f68317102164fa86
Gleb Kolebanov <grkolebanov@edu.hse.ru>
2024-07-12 01:30:16 +0300

    Add some storage methods

--- cmd/urlShortener/main.go
+++ cmd/urlShortener/main.go
@@ -1,18 +1,64 @@
 package main
 
 import (
+	"fmt"
+	"log/slog"
+	"os"
+
 	"github.com/GlebusDev/urlShortener/internal/config"
+	"github.com/GlebusDev/urlShortener/internal/lib/logger/sl"
+	"github.com/GlebusDev/urlShortener/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	// config cleanenv
 	var config = config.MustLoad()
 
+	fmt.Print(config)
+
 	// logger slog
+	var logger = setupLogger(config.Env)
+
+	logger.Info("starting app ", slog.String("env", config.Env))
+	logger.Debug("Debug messages are enabled")
 
 	// storage sqlite
+	strg, err := sqlite.New(config.StoragePath)
 
+	if err != nil {
+		logger.Error("Failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	outUrl, err := strg.GetURL("shot")
+
+	if err != nil {
+		logger.Error("Faild to get url", sl.Err(err))
+	}
+
+	logger.Info("URL: ", outUrl)
 	// router chi
 
 	// server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
---
+++ internal/storage/sqlite/sqlite.go
@@ -0,0 +1,86 @@
+package sqlite
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/GlebusDev/urlShortener/internal/storage"
+	"github.com/mattn/go-sqlite3"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(storagePath string) (*Storage, error) {
+	const op = "storage.sqlite.New"
+
+	db, err := sql.Open("sqlite3", storagePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	stmt, err := db.Prepare(`
+		CREATE TABLE IF NOT EXISTS URL(
+			id INTEGER PRIMARY KEY,
+			alias TEXT NOT NULL UNIQUE,
+			url TEXT NOT NULL);
+		CREATE INDEX IF NOT EXISTS  idx_alias ON url(alias);
+	`)
+
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	_, err = stmt.Exec()
+
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	storage := Storage{db: db}
+
+	return &storage, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) error {
+	const op = " storage.sqlite.SaveURL"
+
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) VALUES(?, ?)")
+
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = stmt.Exec(urlToSave, alias)
+
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return fmt.Errorf("%s: %w", op, storage.ErrURLExists)
+		}
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+func (s *Storage) GetURL(alias string) (string, error) {
+	const op = "storage.sqlite.GetURL"
+
+	stmt, err := s.db.Prepare("SELECT url FROM URL WHERE alias = ?;")
+
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	queryRes := stmt.QueryRow(alias)
+	var resUrl string
+	err = queryRes.Scan(&resUrl)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", fmt.Errorf("%s: %w", op, storage.ErrURLNotFound)
+		}
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return resUrl, nil
+}
